8950752412c495953f5e65e7fcdd98eae0c68380
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2023-12-09 02:29:44 +0500

    1

---
+++ task6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  cout<<"W\tW\tEEEEE\tL\tCCCCCCCC\tOOOOOOOOOO\tMM      MM\tEEEEEEE\n";
+  cout<<"W\tW\tE\tL\tC\t\tOO\tOO\tM M    M M\tE\n";
+  cout<<"W\tW\tE\tL\tC\t\tOO\tOO\tM  M  M  M\tE\n";
+  cout<<"W\tW\tE\tL\tC\t\tOO\tOO\tM   M\t M\tE\n";
+  cout<<"W\tW\tEEEE\tL\tC\t\tOO\tOO\tM\t M\tEEEEE\n";
+  cout<<"W   W   W\tE\tL\tC\t\tOO\tOO\tM\t M\tE\n";
+  cout<<"W  W W  W\tE\tL\tC\t\tOO\tOO\tM\t M\tE\n";
+  cout<<"W W   W W\tE\tL\tC\t\tOO\tOO\tM\t M\tE\n";
+  cout<<"WW     WW\tEEEEE\tLLLLLL\tCCCCCCCC\tOOOOOOOOOO\tM\t M\tEEEEEEE\n";
+  return 0;
+}
